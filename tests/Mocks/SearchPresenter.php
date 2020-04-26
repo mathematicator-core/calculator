cc1afca52aa3b5b3e9cdc256a247541abdb40a4f
Miloš Havlíček <miloshavlicek@gmail.com>
2020-04-26 14:10:13 +0200

    Basic setup for tests, added .gitignore and .editorconfig for faster IDE setup

---
+++ tests/Mocks/SearchPresenter.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\FrontModule\Presenters;
+
+use Nette\Application\IPresenter;
+use Nette\Application\UI\Presenter;
+
+class SearchPresenter extends Presenter implements IPresenter
+{
+	public function actionSearch(...$args)
+	{
+
+	}
+}
