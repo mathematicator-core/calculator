47779b147c3badc50225d327f0c3bad9af23a7b5
Jan Barášek <janbarasek@gmail.com>
2020-10-10 21:15:09 +0200

    Require Nette DI, because common PSR interface is not stable package version.

--- src/Step/StepEndpoint.php
+++ src/Step/StepEndpoint.php
@@ -5,19 +5,20 @@ declare(strict_types=1);
 namespace Mathematicator\Calculator\Step;
 
 
+use Mathematicator\Calculator\Step\Controller\IStepController;
 use Mathematicator\Engine\Exception\TerminateException;
 use Mathematicator\Engine\Step\Step;
+use Nette\DI\Container;
 use Nette\Utils\ArrayHash;
-use Psr\Container\ContainerInterface;
 
 final class StepEndpoint
 {
 
-	/** @var ContainerInterface */
+	/** @var Container */
 	private $container;
 
 
-	public function __construct(ContainerInterface $container)
+	public function __construct(Container $container)
 	{
 		$this->container = $container;
 	}
@@ -28,17 +29,20 @@ final class StepEndpoint
 	 */
 	public function getStep(string $type, string $data): array
 	{
-		$callback = $this->container->get($type);
+		$callback = $this->container->getByType($type);
+		if (!$callback instanceof IStepController) {
+			throw new \RuntimeException('Service must be instance of "' . IStepController::class . '", but type "' . $type . '" given.');
+		}
 
 		try {
-			$data = json_decode($data);
+			$data = \json_decode($data);
 			$arrayHash = new ArrayHash();
 			foreach ($data as $k => $v) {
 				$arrayHash->{$k} = $v;
 			}
 			$steps = $callback->actionDefault($arrayHash);
 		} catch (TerminateException $e) {
-			$steps[] = StepFactory::addStep('Nepodařilo se najít postup pro [' . $type . ']');
+			$steps[] = StepFactory::addStep('Could not find a step-by-step solution for "' . $type . '".');
 		}
 
 		$return = [];
